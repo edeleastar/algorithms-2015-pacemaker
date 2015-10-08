8144f1b0666a43850b411187d652f9667ef0c784
Eamonn de Leastar <edeleastar@wit.ie>
2015-10-08 16:59:36 +0100

    More comprenhenaive createActivity tests

--- test/models/PacemakerAPITest.java
+++ test/models/PacemakerAPITest.java
@@ -19,6 +19,10 @@ public class PacemakerAPITest
   public void setup()
   {
     pacemaker = new PacemakerAPI();
+    for (User user : users)
+    {
+      pacemaker.createUser(user.firstName, user.lastName, user.email, user.password);
+    }
   }
 
   @After
@@ -26,25 +30,81 @@ public class PacemakerAPITest
   {
     pacemaker = null;
   }
-  
-  @Test
-  public void testUserEmpty()
-  {
-    assertEquals (0, pacemaker.getUsers().size());
-  } 
-  
+
   @Test
   public void testUser()
   {
-    User homer = new User ("homer", "simpson", "homer@simpson.com",  "secret");
-    
-    assertEquals (0, pacemaker.getUsers().size());
+    assertEquals (users.length, pacemaker.getUsers().size());
     pacemaker.createUser("homer", "simpson", "homer@simpson.com", "secret");
-    assertEquals (1, pacemaker.getUsers().size());
-    
-    assertEquals (homer, pacemaker.getUserByEmail("homer@simpson.com"));
+    assertEquals (users.length+1, pacemaker.getUsers().size());
+    assertEquals (users[0], pacemaker.getUserByEmail(users[0].email));
+  }  
+
+  @Test
+  public void testUsers()
+  {
+    assertEquals (users.length, pacemaker.getUsers().size());
+    for (User user: users)
+    {
+      User eachUser = pacemaker.getUserByEmail(user.email);
+      assertEquals (user, eachUser);
+      assertNotSame(user, eachUser);
+    }
+  }
+
+  @Test
+  public void testDeleteUsers()
+  {
+    assertEquals (users.length, pacemaker.getUsers().size());
+    User marge = pacemaker.getUserByEmail("marge@simpson.com");
+    pacemaker.deleteUser(marge.id);
+    assertEquals (users.length-1, pacemaker.getUsers().size());    
+  }  
+  
+  @Test
+  public void testAddActivity()
+  {
+    User marge = pacemaker.getUserByEmail("marge@simpson.com");
+    Activity activity = pacemaker.createActivity(marge.id, activities[0].type, activities[0].location, activities[0].distance);
+    Activity returnedActivity = pacemaker.getActivity(activity.id);
+    assertEquals(activities[0],  returnedActivity);
+    assertNotSame(activities[0], returnedActivity);
   }
   
+  @Test
+  public void testAddActivityWithSingleLocation()
+  {
+    User marge = pacemaker.getUserByEmail("marge@simpson.com");
+    Long activityId = pacemaker.createActivity(marge.id, activities[0].type, activities[0].location, activities[0].distance).id;
+
+    pacemaker.addLocation(activityId, locations[0].latitude, locations[0].longitude);
+
+    Activity activity = pacemaker.getActivity(activityId);
+    assertEquals (1, activity.route.size());
+    assertEquals(0.0001, locations[0].latitude,  activity.route.get(0).latitude);
+    assertEquals(0.0001, locations[0].longitude, activity.route.get(0).longitude);   
+  }
   
+  @Test
+  public void testAddActivityWithMultipleLocation()
+  {
+    User marge = pacemaker.getUserByEmail("marge@simpson.com");
+    Long activityId = pacemaker.createActivity(marge.id, activities[0].type, activities[0].location, activities[0].distance).id;
+
+    for (Location location : locations)
+    {
+      pacemaker.addLocation(activityId, location.latitude, location.longitude);      
+    }
+
+    Activity activity = pacemaker.getActivity(activityId);
+    assertEquals (locations.length, activity.route.size());
+    int i = 0;
+    for (Location location : activity.route)
+    {
+      assertEquals(location, locations[i]);
+      i++;
+    }
+  } 
 }
+
 
